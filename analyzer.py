d5c30a34bdc3cd2860c7bd9491426565df3249bc
burkap <brkpyzn@gmail.com>
2020-07-02 13:56:01 +0300

    Removed plotting parts / added boundary checks for iteration.

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,10 @@
 from utils.replay_parser import ReplayParser
 from utils.beatmap import Beatmap
-import numpy as np
-import matplotlib.pyplot as plt
 import time
+
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
 
 
 class Analyzer:
@@ -15,6 +17,9 @@ class Analyzer:
     def __init__(self, replay_file: str, beatmap_file: str):
         self.play_parser = ReplayParser(replay_file)
         self.beatmap_parser = Beatmap(beatmap_file)
+
+        self._frames_count = len(self.play_parser.frames)
+        self._hitobjects_count = len(self.beatmap_parser.hitobjects)
 
         self.current_frame_index = 0
         self.current_hitobject_index = 0
@@ -30,6 +35,8 @@ class Analyzer:
         self.count100 = 0
         self.count50 = 0
         self.countmiss = 0
+
+        self.running = True
         # set cs
         cs = self.beatmap_parser.difficulty["CircleSize"]
         if self.play_parser.mods & 2:  # easy
@@ -59,36 +66,40 @@ class Analyzer:
             self.hit_100 = self.hit_100 * 2/3
             self.hit_300 = self.hit_300 * 2/3
 
+        # 3
+        # Drawing members
+
+        self.d_frame_count = 10
         ####################################################
         # plotting members
-        self.p_frame_count = 10
-        self.fig, self.ax = plt.subplots()
-        self.p_lines = []
-        for _ in range(self.p_frame_count):
-            self.p_lines.append(self.ax.plot(0, 0, 'b-')[0])
-        self.p_hitcircle = plt.Circle((0, 0), self.circle_radius, color='k')
-
         self.frames_x = []
         self.frames_y = []
-        # plotting init
-        plt.ion()
-        self.ax.set_xlim([0, 512])
-        self.ax.set_ylim([0, 384])
-        self.ax.add_artist(self.p_hitcircle)
         ####################################################
+
+    def switch_running(self):
+        self.running = not self.running
 
     def get_relative_frame(self, r_index: int):
         # TODO: add boundary checks
         return self.play_parser.frames[self.current_frame_index+r_index]
 
+    def go_to_prev_frame(self):
+        self.current_frame_index = clamp(
+            self.current_frame_index-1, 0, self._frames_count-1)
+        self.prev_frame = self.play_parser.frames[self.current_frame_index-1]
+        self.current_frame = self.play_parser.frames[self.current_frame_index]
+
     def go_to_next_frame(self):
-        self.prev_frame = self.current_frame
-        self.current_frame_index += 1
+        self.current_frame_index = clamp(
+            self.current_frame_index+1, 0, self._frames_count-1)
+        self.prev_frame = self.play_parser.frames[self.current_frame_index-1]
         self.current_frame = self.play_parser.frames[self.current_frame_index]
 
     def go_to_next_hitobject(self):
-        self.prev_hitobject = self.current_hitobject
-        self.current_hitobject_index += 1
+        self.current_hitobject_index = clamp(
+            self.current_hitobject_index+1, 0, self._hitobjects_count-1)
+        self.prev_hitobject = self.beatmap_parser.hitobjects[self.current_hitobject_index-1]
+        print(self.current_hitobject_index)
         self.current_hitobject = self.beatmap_parser.hitobjects[self.current_hitobject_index]
 
     def check_if_hit(self, frame, hitobject):
@@ -112,49 +123,29 @@ class Analyzer:
             # hit 300
             self.count300 += 1
 
-    def draw_plot(self):
-        self.frames_x.append(self.current_frame.x)
-        self.frames_y.append(self.current_frame.y)
-        self.frames_x = self.frames_x[-self.p_frame_count:]
-        self.frames_y = self.frames_y[-self.p_frame_count:]
-
-        for i in range(self.p_frame_count):
-            self.p_lines[i].remove()
-            self.p_lines[i], = self.ax.plot(self.frames_x, self.frames_y, 'b-')
-
-        self.p_hitcircle.center = self.current_hitobject.x, 384-self.current_hitobject.y
-        if self.check_if_hit(self.current_frame, self.current_hitobject):
-            self.p_hitcircle.set_color('g')
-        else:
-            self.p_hitcircle.set_color('b')
-        plt.show()
-        plt.pause(0.0001)
-
-    def analyze_for_relax(self, draw_plot: bool, show_outside: bool):
+    def analyze_for_relax(self):
         """
             draw_plot:      bool-- ...
             show_outside:   bool-- ...
 
         """
+
         f = open("out.txt", "w")
-        if show_outside:
-            self.ax.set_xlim([-128, 512 + 128])
-            self.ax.set_ylim([-64 * 3 / 2, 384 + 64 * 3 / 2])
         while True:
-            if draw_plot:
-                self.draw_plot()
             print("Current frame: {} | Next hitobject: {} | Previous hitobject: {}".format(
                 self.current_frame.time, self.current_hitobject.time, self.prev_hitobject.time), file=f)
-            if self.prev_frame.k1_pressed and self.current_frame.k1_pressed:
-                pass
-            if not self.prev_frame.k1_pressed and self.current_frame.k1_pressed:
-                self.current_frame_hit = self.check_if_hit(
-                    self.current_frame, self.current_hitobject)
-                if self.current_frame_hit:
-                    self.get_ms_delay(self.current_frame,
-                                      self.current_hitobject)
-            if self.current_frame.time < self.current_hitobject.time:
-                self.go_to_next_frame()
-            else:
-                self.go_to_next_frame()
-                self.go_to_next_hitobject()
+
+            if self.running:
+                if self.prev_frame.k1_pressed and self.current_frame.k1_pressed:
+                    pass
+                if not self.prev_frame.k1_pressed and self.current_frame.k1_pressed:
+                    self.current_frame_hit = self.check_if_hit(
+                        self.current_frame, self.current_hitobject)
+                    if self.current_frame_hit:
+                        self.get_ms_delay(self.current_frame,
+                                          self.current_hitobject)
+                if self.current_frame.time < self.current_hitobject.time:
+                    self.go_to_next_frame()
+                else:
+                    self.go_to_next_frame()
+                    self.go_to_next_hitobject()
