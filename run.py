7d44efe023977de37bda059c2c9ad9ce9d03496f
aticie <efehanatici@gmail.com>
2020-07-09 22:42:06 +0300

    Merge remote-tracking branch 'origin/burkadev' into burkadev
    # Conflicts:
    #	utils/analyzer.py

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from utils.analyzer import Analyzer
 
 cwd = os.getcwd()
 data_folder = os.path.join(cwd, "data")
-replay_file = os.path.join(data_folder, "ronald.osr")
-bmap_file = os.path.join(data_folder, "lady.osu")
+replay_file = os.path.join(data_folder, "whitecat.osr")
+bmap_file = os.path.join(data_folder, "whitecat.osu")
 an = Analyzer(replay_file, bmap_file)
 
 an.run()
